b19ff793429e639f6ec9338a602499d44cc14b74
Nelsontxn <nelsontxn@gmail.com>
2021-07-26 16:34:54 +0800

    Added the basic js hover color change script, works as intended need to add buttons for different color afterwards

--- javascript.js
+++ javascript.js
@@ -1,12 +1,24 @@
-let gridsize = 16
+let gridsize = 64;
 
 function CreateGrid(num){
     for(i = 0; i<num**2; i++){
         let MultiCell = document.createElement('div');
         MultiCell.className = "IndivCell";
+        MultiCell.setAttribute("id",`IndivCell_${i}`)
         document.getElementById('MainBoard').appendChild(MultiCell);
     }
     document.getElementById('MainBoard').style.gridTemplateColumns = `repeat(${num}, 1fr)`
 }
 
 window.onload = CreateGrid(gridsize);
+
+const parent = document.getElementById('MainBoard').children
+const idArray = Array.from(parent).map(x => x.id);
+
+console.log(idArray)
+
+idArray.forEach(element =>{
+    document.getElementById(`${element}`).addEventListener('mouseenter',(e)=>{
+        e.target.style.backgroundColor = 'black';
+    });
+})
