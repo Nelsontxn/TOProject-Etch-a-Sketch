7a39fd93d331b912488688ca5a409ddf440853ad
Nelsontxn <nelsontxn@gmail.com>
2021-07-28 16:37:00 +0800

    Need to read up more on colors and how global variable works

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-
+var color = 'black';
 
 
 //Create Grid//
@@ -10,17 +10,7 @@ function CreateGrid(num){
         document.getElementById('MainBoard').appendChild(MultiCell);
     }
     document.getElementById('MainBoard').style.gridTemplateColumns = `repeat(${num}, 1fr)`;
-    
-    const parent = document.getElementById('MainBoard').children
-    const idArray = Array.from(parent).map(x => x.id);
-
-    console.log(idArray)
-
-    idArray.forEach(element =>{
-        document.getElementById(`${element}`).addEventListener('mouseenter',(e)=>{
-            e.target.style.backgroundColor = 'black';
-    });
-})
+    Hoverfunc()
 
 }
 
@@ -28,18 +18,21 @@ CreateGrid(16);
 
 
 // Hover function//
+function Hoverfunc(){
+    const parent = document.getElementById('MainBoard').children
+    const idArray = Array.from(parent).map(x => x.id);
 
+    idArray.forEach(element =>{
+        document.getElementById(`${element}`).addEventListener('mouseenter', colorGrid)
+});
+}
 
 
 //Reset Function
 document.getElementById('reset').addEventListener('click', resetgrid)
 
 function resetgrid(){
-    const parent = document.getElementById('MainBoard').children
-    const idArray = Array.from(parent).map(x => x.id);
-    idArray.forEach(element =>{
-        document.getElementById(`${element}`).style.backgroundColor = 'white';
-    });
+
     var AlertSize = prompt("Please enter the gridsize", "16");
     gridsize = CorrectSize((Number(AlertSize)));
     deleteGrid()
@@ -61,4 +54,61 @@ function deleteGrid(){
     idArray.forEach(element =>{
         document.getElementById(`${element}`).remove();
 });
+}
+
+//Colors function
+
+const SelectedOptions = document.querySelectorAll('.effects');
+
+SelectedOptions.forEach(options => 
+    
+    options.addEventListener('click', changeColor)
+    );
+
+
+function changeColor(event) {
+    switch (event.target.id) { 
+        case 'rainbow':
+            color = 'rainbow';
+            break;  
+        case 'gray':
+            color = 'gray';
+            break;
+        case 'eraser':
+            color = 'eraser';
+            break;
+        default:
+            color = 'black';
+            break;
+    } 
+}
+
+function colorGrid() {
+    switch (color) {
+        case 'rainbow':
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+            this.classList.remove('gray');
+            break;  
+        case 'gray':
+            if (this.style.backgroundColor.match(/rgba/)) {
+                let currentOpacity = Number(this.style.backgroundColor.slice(-4, -1));
+                if (currentOpacity <= 0.9) {
+                    this.style.backgroundColor = `rgba(0, 0, 0, ${currentOpacity + 0.1})`;
+                    this.classList.add('gray');
+                }
+            } else if (this.classList == 'gray' && this.style.backgroundColor == 'rgb(0, 0, 0)') {
+                return;
+            } else {
+                this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';  
+            }
+            break;
+        case 'eraser':
+            this.style.backgroundColor = '#ffffff';
+            this.classList.remove('gray');
+            break;
+        default:
+            this.style.backgroundColor = color;
+            this.classList.remove('gray');
+            break;
+    } 
 }
